7d0423e81e8484645a614c4e08c1946fb7319fba
feizhen <sysulz19920512@163.com>
2015-12-29 01:58:34 +0800

    initial with test suit

--- builds/js/test.js
+++ builds/js/test.js
@@ -1,7 +1,36 @@
-var hello;
+var assert, log, results, test;
 
-hello = function() {
-  return console.log("hello!");
+results = void 0;
+
+log = function() {
+  var error, error1, error2;
+  try {
+    return console.log.apply(console, arguments);
+  } catch (error1) {
+    error = error1;
+    try {
+      return opera.postError.apply(opera, arguments);
+    } catch (error2) {
+      error = error2;
+      return alert(Array.prototype.join.call(arguments, " "));
+    }
+  }
 };
 
-hello();
+assert = function(value, desc) {
+  var li;
+  li = document.createElement("li");
+  li.className = value ? "pass" : "fail";
+  li.appendChild(document.createTextNode(desc));
+  results.appendChild(li);
+  if (!value) {
+    li.parentNode.parentNode.className = "fail";
+  }
+  return li;
+};
+
+test = function(name, fn) {
+  results = document.getElementById("results");
+  results = assert(true, name).appendChild(document.createElement("ul"));
+  return fn();
+};
--- src/coffee/test.coffee
+++ src/coffee/test.coffee
@@ -1,4 +1,24 @@
-hello = () ->
-  console.log("hello!");
+results = undefined
+log = ()->
+  try
+    console.log.apply(console, arguments);
+  catch error
+    try
+      opera.postError.apply(opera, arguments);
+    catch error
+      alert Array::join.call(arguments, " ")
 
-hello()
+assert = (value, desc)->
+  li = document.createElement "li"
+  li.className = if value then "pass" else "fail"
+  li.appendChild(document.createTextNode desc)
+  results.appendChild(li)
+  if not value
+    li.parentNode.parentNode.className = "fail"
+  li
+
+test = (name, fn)->
+  results = document.getElementById "results"
+  results = assert(true, name)
+          .appendChild document.createElement "ul"
+  fn()
